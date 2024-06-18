74e82c70e57c4c5f815a44974ac59acb090160ca
Dimple Kumari <114878264+Dimple-01@users.noreply.github.com>
2024-06-18 23:40:55 +0530

    Time: 26 ms (10.28%) | Memory: 24.7 MB (13.96%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(auto & x: nums)
+        int n= nums.size();
+        int st=1,end = n-2,mid;
+        if(n == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        while(st <= end)
         {
-            mp[x]++;
-        }
-        for(auto &y:mp)
-        {
-            if(y.second == 1)
-                return y.first;
+            mid = st + ( end - st)/2;
+           
+            if (nums[mid] != nums[mid -1] && nums[mid] != nums[mid + 1]) return nums[mid];
+            
+            else if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 ==0 && nums[mid] == nums[mid +1]))
+            {
+                // checking ele. at even odd same or not
+                    st= mid +1; // eliminate left half
+
+            }  
+            
+            else
+            {
+                
+                    end = mid -1;   // eliminate right half
+            }
         }
         return -1;
-
     }
 };
